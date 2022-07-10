13f71a7d4a346df66c925bca8b6605a6e1a9f242
LinranWu <wulinran20@gmail.com>
2022-07-10 16:55:25 -0400

    finished shipment_data_0

--- populater.py
+++ populater.py
@@ -9,13 +9,20 @@ connection = sqlite3.connect("shipment_database.db")
 cursor = connection.cursor()
 
 product = {}
-i = 0
+prodId = 0
+shipId = 0
+for row in reader0:
+    if not row["product"] in product:
+        product[row["product"]] = prodId
+        cursor.executescript("insert into product (id, name)  values (" + str(prodId) + ", \'" + row["product"] + "\');")
+        prodId+=1
+    cursor.executescript("insert into shipment (id, product_id, quantity, origin, destination)  values (" + str(shipId) + "," + str(product.get(row["product"])) + "," + row["product_quantity"]+", \'" + row["origin_warehouse"] + "\', \'"+ row["destination_store"]+"\');")
+    shipId+=1
 for row in reader1:
     if not row["product"] in product:
-        product[row["product"]] = i
-        print("insert into product (id, name)  values (" + str(i) + ", \'" + row["product"] + "\');")
-        cursor.executescript("insert into product (id, name)  values (" + str(i) + ", \'" + row["product"] + "\');")
-        i+=1
+        product[row["product"]] = prodId
+        cursor.executescript("insert into product (id, name)  values (" + str(prodId) + ", \'" + row["product"] + "\');")
+        prodId+=1
 
 
 
